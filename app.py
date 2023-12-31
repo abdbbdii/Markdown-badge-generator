b4e4152d62cb6968e3afef0b3b8dacff58ce714d
Abdul Rahman <ar69king@gmail.com>
2023-12-31 13:14:32 +0500

    a

--- app.py
+++ app.py
@@ -31,34 +31,34 @@ class Link:
             link_parts.append("?")
             link_parts.extend([f"{key}={value}&" for key, value in self.parameters.items() if value is not None])
         return "".join(link_parts).rstrip("&")
-with st.container():
+with st.container(border=True):
     st.title("Badge Generator")
-label = st.text_input("Label", placeholder="Label", value="Label")
-if not label:
-    st.error("Label cannot be empty.")
-message = st.text_input("Message", placeholder="Message")
-color = st.color_picker("Choose text background color", value="#ffffff")[1:]
-    
-link = Link(label=label,message=message, color=color)
+    label = st.text_input("Label", placeholder="Label", value="Label")
+    if not label:
+        st.error("Label cannot be empty.")
+    message = st.text_input("Message", placeholder="Message")
+    color = st.color_picker("Choose text background color", value="#ffffff")[1:]
+        
+    link = Link(label=label,message=message, color=color)
 
-link.config("style", st.selectbox("Style", ("flat", "flat-square", "plastic", "for-the-badge", "social")))
+    link.config("style", st.selectbox("Style", ("flat", "flat-square", "plastic", "for-the-badge", "social")))
 
-if st.checkbox("Include a logo?"):
-    logo = st.selectbox("Logo", icons)
-    link.config('logo', logo)
-    logoColor = st.color_picker("Choose logo color", value="#000000" if link.get("logoColor") is None else "#"+link.get("logoColor"))[1:]
-    if st.button("Same as text"):
-        logoColor = link.get("color")
-    if st.button("Logo color"):
-        icon = icons.get(link.get("logo"))
-        logoColor = icon.__dict__["hex"]
-    link.config('logoColor', logoColor)
-else:
-    logo = st.text_input("Logo", disabled=True)
-    logoColor = st.color_picker("Choose logo color", disabled=True)
-    st.button("Same as text", disabled=True)
-    st.button("Logo color", disabled=True)
-md=f"![{link.get('label')}]({link})"
-if (link.get("label")):
-    st.markdown(md)
-    st.code(md, "None")
+    if st.checkbox("Include a logo?"):
+        logo = st.selectbox("Logo", icons)
+        link.config('logo', logo)
+        logoColor = st.color_picker("Choose logo color", value="#000000" if link.get("logoColor") is None else "#"+link.get("logoColor"))[1:]
+        if st.button("Same as text"):
+            logoColor = link.get("color")
+        if st.button("Logo color"):
+            icon = icons.get(link.get("logo"))
+            logoColor = icon.__dict__["hex"]
+        link.config('logoColor', logoColor)
+    else:
+        logo = st.text_input("Logo", disabled=True)
+        logoColor = st.color_picker("Choose logo color", disabled=True)
+        st.button("Same as text", disabled=True)
+        st.button("Logo color", disabled=True)
+    md=f"![{link.get('label')}]({link})"
+    if (link.get("label")):
+        st.markdown(md)
+        st.code(md, "None")
